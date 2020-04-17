e66cca50d0f14ec710aa6ebf70a41e97c67245bd
dxl <64101226@qq.com>
2020-04-17 17:12:00 +0800

    upload

---
+++ iobridges/src/main/java/com/iobridges/com/Communication.java
@@ -0,0 +1,21 @@
+package cn.dxl.com;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+/*
+ * author DXL
+ * 通信接口
+ */
+public interface Communication extends Serializable {
+    /**
+     * Get OutputStream implement!
+     */
+    OutputStream getOutput();
+
+    /*
+     * Get InputStream implement!
+     * */
+    InputStream getInput();
+}
